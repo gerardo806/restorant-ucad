7035aebb422159a951672a155c2f0e4e6cf4829e
Anaya <anayagerardo273@gmail.com>
2024-05-29 04:42:32 -0600

    login

--- app/templates/login.php
+++ app/templates/login.php
@@ -31,9 +31,9 @@
 <body class="hold-transition login-page">
     <main class="main" id="top">
         <nav class="navbar navbar-expand-lg fixed-top navbar-dark" data-navbar-on-scroll="data-navbar-on-scroll">
-            <div class="container"><a class="navbar-brand" href="index.html">   
-            Joya's Restaurant
-            </a>
+            <div class="container"><a class="navbar-brand" href="index.html">
+                    Joya's Restaurant
+                </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="Toggle navigation"><i
@@ -45,31 +45,61 @@
                         <li class="nav-item"><a class="nav-link" aria-current="page" href="#vacantes">Empleos</a></li>
                         <li class="nav-item"><a class="nav-link" aria-current="page" href="#menus">Menús</a></li>
                         <li class="nav-item mt-2 mt-lg-0"><a
-                                class="nav-link btn btn-light text-black w-md-25 w-50 w-lg-100" aria-current="page"
-                                href="#">Log In</a></li>
+                                class="nav-link btn btn-light text-black w-md-25 w-50 w-lg-100" data-bs-toggle="modal"
+                                data-bs-target="#staticBackdrop" href="#">Iniciar Sesión</a></li>
                     </ul>
                 </div>
             </div>
         </nav>
 
-            <?php
+        <?php
         @include(MODULO_PATH . "/" . $conf[$modulo]['archivo']);
       ?>
-        </main>
+    </main>
 
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Completar formulario</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label h2">Usuario</label>
+                            <input type="text" class="form-control p-2" id="exampleInputEmail1"
+                                aria-describedby="emailHelp">
+                            <div id="emailHelp" class="form-text"></div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+                            <input type="password" class="form-control p-2" id="exampleInputPassword1">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-info p-2 text-white">Iniciar Sesión</button>
+                    <button type="button" class="btn btn-secondary p-2" data-bs-dismiss="modal">Cerrar</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
-        <script src="resources/vendors/popper/popper.min.js"></script>
-        <script src="resources/vendors/bootstrap/bootstrap.min.js"></script>
-        <script src="resources/vendors/anchorjs/anchor.min.js"></script>
-        <script src="resources/vendors/is/is.min.js"></script>
-        <script src="resources/vendors/fontawesome/all.min.js"></script>
-        <script src="resources/vendors/lodash/lodash.min.js"></script>
-        <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
-        <script src="resources/vendors/prism/prism.js"></script>
-        <script src="resources/vendors/swiper/swiper-bundle.min.js"></script>
-        <script src="resources/dist/js/theme.js"></script>
-        <!--sweetalert-->
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="resources/vendors/popper/popper.min.js"></script>
+    <script src="resources/vendors/bootstrap/bootstrap.min.js"></script>
+    <script src="resources/vendors/anchorjs/anchor.min.js"></script>
+    <script src="resources/vendors/is/is.min.js"></script>
+    <script src="resources/vendors/fontawesome/all.min.js"></script>
+    <script src="resources/vendors/lodash/lodash.min.js"></script>
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=window.scroll"></script>
+    <script src="resources/vendors/prism/prism.js"></script>
+    <script src="resources/vendors/swiper/swiper-bundle.min.js"></script>
+    <script src="resources/dist/js/theme.js"></script>
+    <!--sweetalert-->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 </body>
 
